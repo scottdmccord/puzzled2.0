a7c83e9e3dab912a30f8f55f3b39891c29563343
Scott McCord <scott.sdm27@gmail.com>
2017-01-30 14:49:32 -0500

    react app functional

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super();
+
+
+  }
+
+  render(){
+    return(
+      <container>
+        <h1> HELLO WORLD </h1>
+      </container>
+    )
+  }
+}
+
+export default App;
