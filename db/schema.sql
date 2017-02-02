9b00f36c6d0be777dafa17df7cf95828f2855e5b
Scott McCord <scott.sdm27@gmail.com>
2017-02-02 14:38:09 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,12 @@ CREATE TABLE puzzles (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
   url TEXT NOT NULL,
+  highscore1 TEXT DEFAULT 'No high score',
+  highscore1_user DEFAULT 'n/a',
+  highscore2 TEXT DEFAULT 'No high score',
+  highscore2_user DEFAULT 'n/a',
+  highscore3 TEXT DEFAULT 'No high score',
+  highscore3_user DEFAULT 'n/a',
 );
 
 CREATE TABLE users (
@@ -12,15 +18,4 @@ CREATE TABLE users (
   password VARCHAR NOT NULL,
   email VARCHAR NOT NULL,
   date_created TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE TABLE highscores (
-  id SERIAL PRIMARY KEY,
-  puzzle_id INT NOT NULL,
-  highscore1 TEXT DEFAULT 'No high score',
-  highscore1_user DEFAULT 'n/a',
-  highscore2 TEXT DEFAULT 'No high score',
-  highscore2_user DEFAULT 'n/a',
-  highscore3 TEXT DEFAULT 'No high score',
-  highscore3_user DEFAULT 'n/a',
 );
