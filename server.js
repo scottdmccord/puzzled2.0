7894f6682164311bbc07977cca47f871a892fa63
Scott McCord <scott.sdm27@gmail.com>
2017-02-03 14:51:24 -0500

    created puzzles route and  model

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const path = require('path');
+const puzzleRouter = require('./routes/puzzles');
 
 const app = express();
 const PORT = process.argv[2] || process.env.PORT || 3000;
@@ -13,5 +14,7 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(bodyParser.json());
+
+app.use('/routes/puzzles', puzzleRouter);
 
 app.listen(PORT, () => console.log('Server is here and listening on ', PORT));
