efb8296340df0b4f937b39fcd57d191fddb4c16f
Scott McCord <scott.sdm27@gmail.com>
2017-02-03 11:19:49 -0500

    created game board

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import style from './Board.css';
+
+class Board extends Component {
+  render(){
+    return(
+      <div className="board-container">
+        <div className="board"></div>
+        <div className="board-button-container">
+          <button className="start button">START</button>
+          <button className="new-puzzle button">NEW PUZZLE</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Board;
--- src/components/GamePage/GamePage.jsx
+++ src/components/GamePage/GamePage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import style from './GamePage.css';
 import InfoPanel from '../InfoPanel/InfoPanel.jsx';
+import Board from '../Board/Board.jsx';
 
 class GamePage extends Component {
   render(){
@@ -8,6 +9,7 @@ class GamePage extends Component {
       <div className="gamepage-container">
         <h1> Game Page </h1>
         <InfoPanel />
+        <Board />
       </div>
     )
   }
