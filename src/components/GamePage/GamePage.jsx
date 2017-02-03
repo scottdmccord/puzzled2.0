d83ac984a8e788a843700149e339f62e6c1667b5
Scott McCord <scott.sdm27@gmail.com>
2017-02-03 10:59:30 -0500

    created info panel on board page

--- src/components/GamePage/GamePage.jsx
+++ src/components/GamePage/GamePage.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import style from './GamePage.css';
+import InfoPanel from '../InfoPanel/InfoPanel.jsx';
 
 class GamePage extends Component {
   render(){
     return(
       <div className="gamepage-container">
         <h1> Game Page </h1>
+        <InfoPanel />
       </div>
     )
   }
---
+++ src/components/InfoPanel/InfoPanel.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import style from './InfoPanel.css';
+
+class GamePage extends Component {
+  render(){
+    return(
+      <div className="infopanel-container">
+        <h2> Info Panel </h2>
+      </div>
+    )
+  }
+}
