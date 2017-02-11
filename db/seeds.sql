223e9a9e7780e0a9cf9237c1519d0fcffb05825d
Scott McCord <scott.sdm27@gmail.com>
2017-02-11 11:34:57 -0500

    added more puzzles to app

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
 INSERT INTO puzzles (name, url) VALUES ('Puzzle 1', 'url("http://i.imgur.com/B8xsff4.jpg?4")');
+INSERT INTO puzzles (name, url) VALUES ('Puzzle 2', 'url("http://i.imgur.com/b5gcmnR.jpg?2")');
+INSERT INTO puzzles (name, url) VALUES ('Puzzle 3', 'url("http://i.imgur.com/CWvP6zh.jpg?3")');
+INSERT INTO puzzles (name, url) VALUES ('Puzzle 4', 'url("http://i.imgur.com/wxiGxy4.jpg?2")');
+INSERT INTO puzzles (name, url) VALUES ('Puzzle 5', 'url("http://i.imgur.com/mGOb37s.jpg?2")');
