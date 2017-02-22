f84576bc40f378c6545db5239f6f9939d47467fb
Scott McCord <scott.sdm27@gmail.com>
2017-02-22 11:29:24 -0500

    created scores model

---
+++ models/scores.js
@@ -0,0 +1,14 @@
+const db = require('../lib/dbConnect.js');
+
+function getHighScores(req, res, next) {
+
+}
+
+
+function submitScore(req, res, next) {
+
+}
+
+module.exports = {
+  getHighScores, submitScore
+}
