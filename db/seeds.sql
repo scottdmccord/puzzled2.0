5ec4286209bed99a7274b4dec480a8354e8a5797
Scott McCord <scott.sdm27@gmail.com>
2017-06-26 16:58:15 -0400

    scores saving successfully

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,23 +4,23 @@ INSERT INTO puzzles (name, url) VALUES ('Puzzle 3', 'url(http://i.imgur.com/CWvP
 INSERT INTO puzzles (name, url) VALUES ('Puzzle 4', 'url(http://i.imgur.com/wxiGxy4.jpg?2)');
 INSERT INTO puzzles (name, url) VALUES ('Puzzle 5', 'url(http://i.imgur.com/mGOb37s.jpg?2)');
 
-INSERT INTO users (username, password, email) VALUES ('Susana', 'test', 'Susana@test.com');
-INSERT INTO users (username, password, email) VALUES ('Scott', 'test', 'Scott@test.com');
-INSERT INTO users (username, password, email) VALUES ('Phil', 'test', 'Phil@test.com');
+INSERT INTO users (username, password, email) VALUES ('Guest', 'n/a', 'no@email.com');
+-- INSERT INTO users (username, password, email) VALUES ('Scott', 'test', 'Scott@test.com');
+-- INSERT INTO users (username, password, email) VALUES ('Phil', 'test', 'Phil@test.com');
 
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (1, '1:00', 1, 1, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (2, '2:00', 2, 1, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (3, '3:00', 3, 1, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (1, '1:00', 1, 2, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (2, '2:00', 2, 2, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (3, '3:00', 3, 2, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (1, '1:00', 1, 3, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (2, '2:00', 2, 3, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (3, '3:00', 3, 3, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (1, '1:00', 1, 4, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (2, '2:00', 2, 4, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (3, '3:00', 3, 4, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (1, '1:00', 1, 5, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (2, '2:00', 2, 5, 'easy');
-INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (3, '3:00', 3, 5, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 1, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 1, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 1, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 2, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 2, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 2, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 3, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 3, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 3, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 4, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 4, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 4, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 5, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 5, 'easy');
+INSERT INTO scores (score, clock, user_id, puzzle_id, difficulty) VALUES (9999999, 'No Score', 1, 5, 'easy');
 
