8c63f658bfb943d3b0c658b0975ca9f310e1321e
Scott McCord <scott.sdm27@gmail.com>
2017-07-09 21:59:57 -0400

    removed ABOUT link for now

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import style from './NavBar.css';
 
+// <div id="about-nav" className="navbar-category">About</div>
 class NavBar extends Component {
   render(){
     return(
       <div className="navbar">
         <div id="home-nav" className="navbar-category"><Link to="/">Home</Link></div>
-        <div id="about-nav" className="navbar-category">About</div>
-        <div id="login-nav" className="navbar-category"><Link to="/login">Log In</Link></div>
         <div id="signup-nav" className="navbar-category"><Link to="/signup">Sign Up</Link></div>
+        <div id="login-nav" className="navbar-category"><Link to="/login">Log In</Link></div>
         <div id="play-nav" className="navbar-category"><Link to="/gamepage">Play</Link></div>
         <div id="logout-nav" className="navbar-category"><Link onClick={this.props.logOut} to="/">Log Out</Link></div>
       </div>
