37012209edc892ff0a6782f1e6b54054d3794321
Scott McCord <scott.sdm27@gmail.com>
2017-08-21 11:46:13 -0500

    testing error message with login

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,8 @@ userRouter.post('/', createUser, showSuccessfulCreateUser, (err, req, res, next)
   res.status(400).send('Username already taken');
 });
 
-userRouter.post('/login', authenticateUser, (req, res, next) => {
+userRouter.post('/login', authenticateUser, (err, req, res, next) => {
+  res.status(400).send('Incorrect username and/or password');
   res.json({message: "successfully signed in"});
 });
 
